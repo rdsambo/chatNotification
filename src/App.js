285c0b6adc9f8beeac006b3a8227dd57d21d1098
RDSambo <rdsambo@gmail.com>
2023-07-05 10:19:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import { useState, useEffect, memo } from 'react';
 import db, { auth } from './firebase';
 import Cookies from 'js-cookie';
-
 function App() {
   const [user, setUser] = useState(null);
-  const [chats, setChats] = useState(null);
-  const [unreadMessages, setUnreadMessages] = useState(0);
-
   useEffect(() => {
     const email = "rdsambo@gmail.zk";
     // const email = Cookies.get("userEmail");
@@ -17,20 +13,13 @@ function App() {
       auth.createUserWithEmailAndPassword(email, pass).then(
       (firebase)=>
       {
-        console.log("value.user.email");
-        console.log();
         firebase.user.updateProfile({
             displayName: username
         }).then(function() {
-            console.log("username atualizado com sucesso!");
             signIn(email, pass);
-        }, function(error) {
-            console.log("Erro ao atulizar username!\nErro:"+error);
         });
-        console.log("usuario criado com sucesso!");
       },
       (reason) => {
-          console.log("Erro ao criar usuario usuario! /nErro:"+reason);
           signIn(email, pass);
       });
     }
@@ -41,35 +30,34 @@ function App() {
       Cookies.set('User', JSON.stringify(firebase.user), { expires: 7, path: '' });
     });
   }
-
   useEffect(() => {
     if (user) {
-      db.collection("users").doc(user.uid).collection("chats").orderBy("timestamp", "desc")
+      db.collection("users").doc(user.uid).collection("chats")
       .onSnapshot({ includeMetadataChanges: true }, snap => {
         if (snap.docs?.length > 0) {
           if (!snap.metadata.fromCache) {
-            const chts = snap.docs.map(cur => ({
-              ...cur.data(),
-              id: cur.id
-            }));
             let quantUnr = 0
-            chts.forEach(cur =>
-              {
-                quantUnr += cur?.unreadMessages !=null? cur?.unreadMessages : 0; 
-              });
-            setUnreadMessages(quantUnr);
-            setChats(chts);
+            snap.docs.forEach(cur => {
+              quantUnr += cur.data()?.unreadMessages !=null? cur.data()?.unreadMessages : 0; 
+            });
+            const badge = document.getElementById("chat_notification_counter");
+            console.log("quantUnr")
+            console.log(quantUnr)
+            if (badge){
+              badge.innerHTML = quantUnr+"";
+              if(quantUnr == 0)
+                badge.className = "display-none badge"
+              else
+                badge.className = "badge"
+            }
           };
-        } else {
-          setChats([]);
         };
       });
     };
   }, [user]);
 
   return (
-    <div id='notfication_counter'>chats:{chats!=null?unreadMessages:0}</div>
+    <div></div>
   );
 }
-
 export default memo(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './App';
-import { StateProvider } from './StateProvider';
-import reducer, { initialState } from "./reducer"
-
 ReactDOM.render(
-  <StateProvider initialState={initialState} reducer={reducer}>
-    <App />
-  </StateProvider>,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
